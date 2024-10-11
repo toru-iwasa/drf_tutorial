233c3c7c7bc4d484a45869f29b9563653fd670ee
toru.iwasa <toru.iwasa@beex-inc.com>
2024-10-11 18:21:55 +0900

    スニペットの変更を所有者に制限

--- drf_tutorial/snippets/views.py
+++ drf_tutorial/snippets/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from rest_framework import generics, permissions
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
+from snippets.permissions import IsOwnerOrReadOnly
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
@@ -14,7 +15,10 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly,
+        IsOwnerOrReadOnly
+    ]
 
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
